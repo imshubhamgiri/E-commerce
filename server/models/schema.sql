eeb1414735e5d19f8c3511472dde3acb86899ca4
imshubhamgiri <skgiri569@gmail.com>
2026-06-25 23:59:56 +0530

    update

--- server/models/schema.sql
+++ server/models/schema.sql
@@ -3,10 +3,13 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
+CREATE TYPE ORDER_STATUS AS ENUM ('pending','confirmed', 'completed', 'cancelled');
+
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -40,8 +43,56 @@ CREATE TABLE orders (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID REFERENCES users(id),
     total_price DECIMAL(10, 2) NOT NULL,
+    status ORDER_STATUS DEFAULT 'pending',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- 5. Create Order Items Table (Fixed REFERENCES typo)
+CREATE TABLE order_items (
+    id UUID PRIMARY KEY DEFAULT
+    gen_random_uuid(),
+    order_id UUID REFERENCES orders(id) ON DELETE CASCADE,
+    product_id UUID REFERENCES products(id) ON DELETE CASCADE,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TYPE PAYMENT_STATUS AS ENUM ('pending', 'completed', 'failed');
+-- 5. Create Payments Table
+CREATE TABLE payments (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    order_id UUID REFERENCES orders(id) ON DELETE CASCADE,
+    amount DECIMAL(10, 2) NOT NULL,
+    payment_method VARCHAR(255),
+    status PAYMENT_STATUS DEFAULT 'pending',
+    razorpayOrderId VARCHAR(255),
+    razorpayPaymentId VARCHAR(255),
+    razorpaySignature VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 6. Create Addresses Table;
+
+CREATE TABLE addresses (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    address TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE orders
+ADD COLUMN address_id UUID REFERENCES addresses(id);
+
+ALTER TABLE orders
+ADD COLUMN  status ORDER_STATUS DEFAULT 'pending'
+
+TRUNCATE TABLE orders RESTART IDENTITY CASCADE;
 
 ALTER TABLE users ADD COLUMN IF NOT EXISTS password VARCHAR(255) NOT NULL;
+
+
+
