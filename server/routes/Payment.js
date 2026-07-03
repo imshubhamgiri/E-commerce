1c6b769323cc2642ed71de9f33ff9e6de9c4cdac
imshubhamgiri <skgiri569@gmail.com>
2026-07-03 18:56:07 +0530

    refactor: replace mongodb function with pg method for payment endpoints

--- server/routes/Payment.js
+++ server/routes/Payment.js
@@ -3,7 +3,7 @@ import * as paymentController from '../controllers/paymentController.js';
 
 const paymentRouter = express.Router();
 
-paymentRouter.post('/create-razorpay-order', paymentController.createRazorpayOrder);
+paymentRouter.post('/create-razorpay-order', paymentController.createPgRazorpayOrder);
 paymentRouter.post('/verify-payment', paymentController.verifyPayment);
 
 export default paymentRouter;
