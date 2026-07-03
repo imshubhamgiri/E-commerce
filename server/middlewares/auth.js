9a8017a45f93d7c843e8a6049b4758aa6a4c4ab9
imshubhamgiri <skgiri569@gmail.com>
2026-07-03 19:01:27 +0530

    refactor: use id instead of _id to follow current structure of jwt payload

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 import dotenv from 'dotenv';
+import pgPool from '../config/pgdb.js';
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -19,7 +20,8 @@ export default async function authMiddleware(req, res, next) {
         const decoded = jwt.verify(token, JWT_SECRET);
         // req.user = { id: decoded.id };
         // next();
-        const user = await User.findById(decoded.userId).select('-password');
+        // const user = await User.findById(decoded.userId).select('-password');
+        const user = await pgPool.query('SELECT * FROM users WHERE id = $1', [decoded.userId]);
         if (!user) {
             return res.status(401).json({
                 success: false,
@@ -27,8 +29,9 @@ export default async function authMiddleware(req, res, next) {
                 message: 'User not found for ' + [decoded]
             });
         }
-        req.user = user;
-        console.log('Authenticated user:', user.id);
+        const decodedUser = user.rows[0];
+        req.user = decodedUser;
+        console.log('Authenticated user:', decodedUser.id);
         next();
     } catch (e) {
         console.error('JWT VERFICATION FAILED', e);
